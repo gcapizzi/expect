cb620f06e5f3d02fed67fec1765dc72e7636059f
Giuseppe Capizzi <g.capizzi@gmail.com>
2019-03-03 23:58:12 +0000

    Actual values should be borrowed

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,15 @@ pub trait Matcher<T> {
     fn match_value(&self, actual: &T) -> Match;
 }
 
-pub fn expect<T>(actual: T) -> Expectation<T> {
+pub fn expect<'a, T>(actual: &'a T) -> Expectation<'a, T> {
     Expectation { actual: actual }
 }
 
-pub struct Expectation<T> {
-    actual: T,
+pub struct Expectation<'a, T: 'a> {
+    actual: &'a T,
 }
 
-impl<T> Expectation<T> {
+impl<'a, T> Expectation<'a, T> {
     pub fn to<M: Matcher<T>>(&self, matcher: M) {
         if let Match::NotMatched(positive_failure_message) = matcher.match_value(&self.actual) {
             panic!(positive_failure_message)
@@ -39,23 +39,30 @@ mod tests {
 
     #[test]
     fn expect_to_should_not_panic_if_the_matcher_matches_successfully() {
-        expect(2 + 2).to(equal(4))
+        expect(&(2 + 2)).to(equal(4))
     }
 
     #[test]
     #[should_panic(expected = "expected 4 to equal 5")]
     fn expect_to_should_panic_if_the_marcher_fails_to_match() {
-        expect(2 + 2).to(equal(5))
+        expect(&(2 + 2)).to(equal(5))
     }
 
     #[test]
     #[should_panic(expected = "expected 4 not to equal 4")]
     fn expect_not_to_should_panic_if_the_marcher_matches_successfully() {
-        expect(2 + 2).not_to(equal(4))
+        expect(&(2 + 2)).not_to(equal(4))
     }
 
     #[test]
     fn expect_not_to_should_not_panic_if_the_matcher_fails_to_match() {
-        expect(2 + 2).not_to(equal(5))
+        expect(&(2 + 2)).not_to(equal(5))
+    }
+
+    #[test]
+    fn it_should_not_take_ownership_of_the_actual_value() {
+        let s = String::from("hello");
+        expect(&s).to(equal(String::from("hello")));
+        expect(&s).not_to(equal(String::from("bye")));
     }
 }
