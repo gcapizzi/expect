29f98ecc527b92da1170f4ce0f38d195d74c8b51
Giuseppe Capizzi <g.capizzi@gmail.com>
2019-03-04 17:55:17 +0000

    Fix typos

--- src/lib.rs
+++ src/lib.rs
@@ -44,13 +44,13 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "expected 4 to equal 5")]
-    fn expect_to_should_panic_if_the_marcher_fails_to_match() {
+    fn expect_to_should_panic_if_the_matcher_fails_to_match() {
         expect(&(2 + 2)).to(equal(5))
     }
 
     #[test]
     #[should_panic(expected = "expected 4 not to equal 4")]
-    fn expect_not_to_should_panic_if_the_marcher_matches_successfully() {
+    fn expect_not_to_should_panic_if_the_matcher_matches_successfully() {
         expect(&(2 + 2)).not_to(equal(4))
     }
 
