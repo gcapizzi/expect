82edeac0e17d998c7cf4c3e00a2d3db4f817256f
Giuseppe Capizzi <gcapizzi@pivotal.io>
2019-03-07 17:21:36 +0000

    be_something -> be_some, use equals otherwise

--- src/matchers/be_some.rs
+++ src/matchers/be_some.rs
@@ -1,31 +1,17 @@
 use crate::Match;
 use crate::Matcher;
 
-pub fn be_some<T>(expected: T) -> SomeMatcher<T> {
-    SomeMatcher { expected: expected }
+pub fn be_some() -> SomeMatcher {
+    SomeMatcher {}
 }
 
-pub struct SomeMatcher<T> {
-    expected: T,
-}
+pub struct SomeMatcher {}
 
-impl<T: std::cmp::PartialEq + std::fmt::Debug> Matcher<Option<T>> for SomeMatcher<T> {
+impl<T: std::fmt::Debug> Matcher<Option<T>> for SomeMatcher {
     fn match_value(&self, actual: &Option<T>) -> Match {
         match actual {
-            Some(actual_value) => {
-                if actual_value == &self.expected {
-                    Match::Matched(format!(
-                        "expected Some({:?}) not to be Some({:?})",
-                        actual_value, self.expected
-                    ))
-                } else {
-                    Match::NotMatched(format!(
-                        "expected Some({:?}) to be Some({:?})",
-                        actual_value, self.expected
-                    ))
-                }
-            }
-            None => Match::NotMatched(format!("expected None to be Some({:?})", self.expected)),
+            Some(_) => Match::Matched(format!("expected {:?} not to be a Some", actual)),
+            _ => Match::NotMatched(String::from("expected None to be a Some")),
         }
     }
 }
@@ -39,34 +25,25 @@ mod tests {
     use crate::Matcher;
 
     #[test]
-    fn should_match_if_actual_is_some_and_value_is_equal_to_expected() {
-        let actual = Some(42);
+    fn should_match_if_actual_is_some() {
+        let actual = Some("foo");
         assert_eq!(
-            SomeMatcher { expected: 42 }.match_value(&actual),
-            Match::Matched(String::from("expected Some(42) not to be Some(42)"))
-        )
-    }
-
-    #[test]
-    fn should_not_match_if_actual_is_some_but_value_is_not_equal_to_expected() {
-        let actual = Some(43);
-        assert_eq!(
-            SomeMatcher { expected: 42 }.match_value(&actual),
-            Match::NotMatched(String::from("expected Some(43) to be Some(42)"))
+            SomeMatcher {}.match_value(&actual),
+            Match::Matched(String::from("expected Some(\"foo\") not to be a Some"))
         )
     }
 
     #[test]
     fn should_not_match_if_actual_is_none() {
-        let actual: Option<u32> = None;
+        let actual: Option<String> = None;
         assert_eq!(
-            SomeMatcher { expected: 42 }.match_value(&actual),
-            Match::NotMatched(String::from("expected None to be Some(42)"))
+            SomeMatcher {}.match_value(&actual),
+            Match::NotMatched(String::from("expected None to be a Some"))
         )
     }
 
     #[test]
     fn be_some_should_contruct_a_some_matcher() {
-        expect(&Some("thing")).to(be_some("thing"))
+        expect(&Some("thing")).to(be_some())
     }
 }
--- src/matchers/mod.rs
+++ src/matchers/mod.rs
@@ -1,5 +1,4 @@
 pub mod be_none;
 pub mod be_ok;
 pub mod be_some;
-pub mod be_something;
 pub mod equal;
