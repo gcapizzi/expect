8a589ae8fd9f68630f1fee9650b8c88df0a496c6
Giuseppe Capizzi <g.capizzi@gmail.com>
2020-04-19 12:29:27 +0200

    Add the exist matcher

--- src/matchers.rs
+++ src/matchers.rs
@@ -4,6 +4,7 @@ mod be_ok;
 mod be_some;
 mod contain;
 mod equal;
+mod exist;
 
 pub use self::be_err::be_err;
 pub use self::be_none::be_none;
@@ -11,3 +12,4 @@ pub use self::be_ok::be_ok;
 pub use self::be_some::be_some;
 pub use self::contain::contain;
 pub use self::equal::equal;
+pub use self::exist::exist;
---
+++ src/matchers/exist.rs
@@ -0,0 +1,58 @@
+use crate::Matcher;
+
+use std::path::Path;
+
+pub fn exist() -> ExistMatcher {
+    ExistMatcher {}
+}
+
+pub struct ExistMatcher {}
+
+impl<T: std::fmt::Debug + AsRef<Path>> Matcher<T> for ExistMatcher {
+    fn match_value(&self, actual: &T) -> bool {
+        actual.as_ref().exists()
+    }
+
+    fn failure_message(&self, actual: &T) -> String {
+        format!("\tExpected:\n\t\t{:?}\n\tto exist", actual)
+    }
+
+    fn negated_failure_message(&self, actual: &T) -> String {
+        format!("\tExpected:\n\t\t{:?}\n\tnot to exist", actual)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::exist;
+    use super::ExistMatcher;
+    use crate::expect;
+    use crate::Matcher;
+
+    #[test]
+    fn should_match_if_actual_exists() {
+        assert!(ExistMatcher {}.match_value(&"./Cargo.toml"))
+    }
+
+    #[test]
+    fn should_not_match_if_actual_is_none() {
+        assert!(!ExistMatcher {}.match_value(&"does_not_exist"))
+    }
+
+    #[test]
+    fn failure_messages() {
+        assert_eq!(
+            ExistMatcher {}.failure_message(&"does_not_exist"),
+            String::from("\tExpected:\n\t\t\"does_not_exist\"\n\tto exist")
+        );
+        assert_eq!(
+            ExistMatcher {}.negated_failure_message(&"does_exist"),
+            String::from("\tExpected:\n\t\t\"does_exist\"\n\tnot to exist")
+        );
+    }
+
+    #[test]
+    fn exist_should_contruct_an_exist_matcher() {
+        expect(&"./Cargo.toml").to(exist())
+    }
+}
