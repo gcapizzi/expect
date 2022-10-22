dba28a8c72fbabf38db2dbbca5a5adcd2b16433e
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-10-22 21:20:33 +0200

    be_empty doesn't need elements to implement any trait
    This splits the old `Collection` trait into two indipendent traits,
    `Container` used by `contain` and `Emptiable` used by `be_empty`.

--- src/matchers/collection.rs
+++ src/matchers/collection.rs
@@ -1,7 +1,5 @@
 use crate::{Description, Matcher};
 
-use std::marker::PhantomData;
-
 /// Matches if `actual` contains `element`.
 ///
 /// Supports [arrays] [`Vec`]s, [`VecDeque`]s, [`LinkedList`]s, [`HashSet`]s and [`BTreeSet`]s.
@@ -28,7 +26,7 @@ pub struct ContainMatcher<T> {
     element: T,
 }
 
-impl<T: std::fmt::Debug, V: Collection<T>> Matcher<V> for ContainMatcher<T> {
+impl<T: std::fmt::Debug, V: Container<T>> Matcher<V> for ContainMatcher<T> {
     fn match_value(&self, collection: &V) -> bool {
         collection.contains_element(&self.element)
     }
@@ -38,6 +36,46 @@ impl<T: std::fmt::Debug, V: Collection<T>> Matcher<V> for ContainMatcher<T> {
             verb: String::from("contain"),
             object: Some(format!("{:?}", self.element)),
         }
+    }
+}
+
+pub trait Container<T> {
+    fn contains_element(&self, element: &T) -> bool;
+}
+
+impl<T: std::cmp::PartialEq, const N: usize> Container<T> for [T; N] {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
+    }
+}
+
+impl<T: std::cmp::PartialEq> Container<T> for std::vec::Vec<T> {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
+    }
+}
+
+impl<T: std::cmp::PartialEq> Container<T> for std::collections::VecDeque<T> {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
+    }
+}
+
+impl<T: std::cmp::PartialEq> Container<T> for std::collections::LinkedList<T> {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
+    }
+}
+
+impl<T: std::cmp::Eq + std::hash::Hash> Container<T> for std::collections::HashSet<T> {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
+    }
+}
+
+impl<T: std::cmp::Ord> Container<T> for std::collections::BTreeSet<T> {
+    fn contains_element(&self, element: &T) -> bool {
+        self.contains(element)
     }
 }
 
@@ -58,17 +96,13 @@ impl<T: std::fmt::Debug, V: Collection<T>> Matcher<V> for ContainMatcher<T> {
 /// # use expect::{expect, matchers::collection::be_empty};
 /// expect(&Vec::<i32>::new()).to(be_empty());
 /// ```
-pub fn be_empty<T>() -> BeEmptyMatcher<T> {
-    BeEmptyMatcher::<T> {
-        phantom: PhantomData,
-    }
+pub fn be_empty() -> BeEmptyMatcher {
+    BeEmptyMatcher {}
 }
 
-pub struct BeEmptyMatcher<T> {
-    phantom: PhantomData<T>,
-}
+pub struct BeEmptyMatcher {}
 
-impl<T, V: Collection<T>> Matcher<V> for BeEmptyMatcher<T> {
+impl<V: Emptiable> Matcher<V> for BeEmptyMatcher {
     fn match_value(&self, collection: &V) -> bool {
         collection.empty()
     }
@@ -81,66 +115,41 @@ impl<T, V: Collection<T>> Matcher<V> for BeEmptyMatcher<T> {
     }
 }
 
-pub trait Collection<T> {
-    fn contains_element(&self, element: &T) -> bool;
+pub trait Emptiable {
     fn empty(&self) -> bool;
 }
 
-impl<T: std::cmp::PartialEq, const N: usize> Collection<T> for [T; N] {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T, const N: usize> Emptiable for [T; N] {
     fn empty(&self) -> bool {
         self.is_empty()
     }
 }
 
-impl<T: std::cmp::PartialEq> Collection<T> for std::vec::Vec<T> {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T> Emptiable for std::vec::Vec<T> {
     fn empty(&self) -> bool {
         self.is_empty()
     }
 }
 
-impl<T: std::cmp::PartialEq> Collection<T> for std::collections::VecDeque<T> {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T> Emptiable for std::collections::VecDeque<T> {
     fn empty(&self) -> bool {
         self.is_empty()
     }
 }
 
-impl<T: std::cmp::PartialEq> Collection<T> for std::collections::LinkedList<T> {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T> Emptiable for std::collections::LinkedList<T> {
     fn empty(&self) -> bool {
         self.is_empty()
     }
 }
 
-impl<T: std::cmp::Eq + std::hash::Hash> Collection<T> for std::collections::HashSet<T> {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T> Emptiable for std::collections::HashSet<T> {
     fn empty(&self) -> bool {
         self.is_empty()
     }
 }
 
-impl<T: std::cmp::Ord> Collection<T> for std::collections::BTreeSet<T> {
-    fn contains_element(&self, element: &T) -> bool {
-        self.contains(element)
-    }
-
+impl<T> Emptiable for std::collections::BTreeSet<T> {
     fn empty(&self) -> bool {
         self.is_empty()
     }
@@ -148,7 +157,7 @@ impl<T: std::cmp::Ord> Collection<T> for std::collections::BTreeSet<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::{be_empty, contain, Collection};
+    use super::{be_empty, contain, Container, Emptiable};
     use crate::Matcher;
 
     #[test]
@@ -186,66 +195,106 @@ mod tests {
     }
 
     #[test]
-    fn arrays_are_collections() {
+    fn arrays_are_containers() {
         assert!([1, 2, 3].contains_element(&2));
+        assert!(![1, 2, 3].contains_element(&4));
+    }
+
+    #[test]
+    fn arrays_are_emptiable() {
+        assert!(([] as [u64; 0]).empty());
         assert!(![1, 2, 3].empty());
     }
 
     #[test]
-    fn vecs_are_collections() {
+    fn vecs_are_containers() {
         assert!(vec![1, 2, 3].contains_element(&2));
+        assert!(!vec![1, 2, 3].contains_element(&4));
+    }
+
+    #[test]
+    fn vecs_are_emptiable() {
         assert!(std::vec::Vec::<i32>::new().empty());
+        assert!(!vec![1, 2, 3].empty());
     }
 
     #[test]
-    fn vecdeques_are_collections() {
+    fn vecdeques_are_containers() {
         let mut numbers = std::collections::VecDeque::new();
-
-        assert!(numbers.empty());
-
         numbers.push_back(1);
         numbers.push_back(2);
         numbers.push_back(3);
 
-        assert!(numbers.contains_element(&2))
+        assert!(numbers.contains_element(&2));
+        assert!(!numbers.contains_element(&4));
     }
 
     #[test]
-    fn linkedlists_are_collections() {
+    fn vecdeques_are_emptiable() {
+        let mut numbers = std::collections::VecDeque::new();
+        assert!(numbers.empty());
+
+        numbers.push_back(42);
+        assert!(!numbers.empty());
+    }
+
+    #[test]
+    fn linkedlists_are_containers() {
         let mut numbers = std::collections::LinkedList::new();
-
-        assert!(numbers.empty());
-
         numbers.push_back(1);
         numbers.push_back(2);
         numbers.push_back(3);
 
-        assert!(numbers.contains_element(&2))
+        assert!(numbers.contains_element(&2));
+        assert!(!numbers.contains_element(&4));
     }
 
     #[test]
-    fn hashsets_are_collections() {
+    fn linkedlists_are_emptiable() {
+        let mut numbers = std::collections::LinkedList::new();
+        assert!(numbers.empty());
+
+        numbers.push_back(42);
+        assert!(!numbers.empty())
+    }
+
+    #[test]
+    fn hashsets_are_containers() {
         let mut numbers = std::collections::HashSet::new();
-
-        assert!(numbers.empty());
-
         numbers.insert(1);
         numbers.insert(2);
         numbers.insert(3);
 
-        assert!(numbers.contains_element(&2))
+        assert!(numbers.contains_element(&2));
+        assert!(!numbers.contains_element(&4));
     }
 
     #[test]
-    fn btreesets_are_collections() {
-        let mut numbers = std::collections::BTreeSet::new();
-
+    fn hashsets_are_emptiable() {
+        let mut numbers = std::collections::HashSet::new();
         assert!(numbers.empty());
 
+        numbers.insert(42);
+        assert!(!numbers.empty())
+    }
+
+    #[test]
+    fn btreesets_are_containers() {
+        let mut numbers = std::collections::BTreeSet::new();
         numbers.insert(1);
         numbers.insert(2);
         numbers.insert(3);
 
-        assert!(numbers.contains_element(&2))
+        assert!(numbers.contains_element(&2));
+        assert!(!numbers.contains_element(&4));
+    }
+
+    #[test]
+    fn btreesets_are_emptiable() {
+        let mut numbers = std::collections::BTreeSet::new();
+        assert!(numbers.empty());
+
+        numbers.insert(42);
+        assert!(!numbers.empty())
     }
 }
